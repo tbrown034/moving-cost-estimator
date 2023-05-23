bc6d89150a37dccaec104df22732e454b26642ea
tbrown034 <tbrown034@gmail.com>
2023-05-23 11:57:07 -0500

    first committ

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <section>
+        <div>
+          <h1>his</h1>
+        </div>
+      </section>
+    </>
+  );
+}
