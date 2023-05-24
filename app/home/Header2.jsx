43f9e290a181fd2b269657bad5ccc83514dee3c2
tbrown034 <tbrown034@gmail.com>
2023-05-23 21:03:44 -0500

    building  my own navbar

--- app/home/Header2.jsx
+++ app/home/Header2.jsx
@@ -1,1 +1,29 @@
-export default function Header2() {}
+"use client";
+import Link from "next/link";
+
+export default function Header2() {
+  return (
+    <nav>
+      <section className=" bg-emerald-800 text-slate-200">
+        <div>
+          <Link
+            href="/"
+            className="flex items-center mb-4 hover:text-gray-900 sm:mb-0 "
+          >
+            <span
+              className="material-symbols-outlined text-slate-100 "
+              style={{ fontSize: "40px" }}
+            >
+              move
+            </span>
+            <span className="self-center pl-3 text-2xl font-semibold whitespace-nowrap ">
+              ShipIt
+            </span>
+          </Link>
+        </div>
+        <div></div>
+        <div></div>
+      </section>
+    </nav>
+  );
+}
--- app/home/Hero.jsx
+++ app/home/Hero.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Hero() {
   return (
     <>
-      <section className="flex flex-col items-center justify-center min-h-screen gap-4 bg-slate-200 text-emerald-800">
-        <div className=" sm:flex sm:justify-center">
+      <section className="flex flex-col items-center justify-center min-h-screen bg-slate-200 text-emerald-800">
+        <div className="">
           <div className="relative px-3 py-1 my-8 text-sm leading-6 rounded-full text-emerald-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
             Using AI-Powered Tech to Fuel Your Move.{" "}
             <Link href="#" className="font-semibold text-emerald-800">
