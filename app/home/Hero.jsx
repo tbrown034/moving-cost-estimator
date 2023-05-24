96f3807e1d769d7a1aae415d46e751227b689575
tbrown034 <tbrown034@gmail.com>
2023-05-23 22:45:30 -0500

    killed a useless div

--- app/home/Hero.jsx
+++ app/home/Hero.jsx
@@ -4,14 +4,12 @@ export default function Hero() {
   return (
     <>
       <section className="flex flex-col items-center justify-center min-h-75vh bg-slate-200 text-emerald-800">
-        <div className="">
-          <div className="relative px-3 py-1 my-8 text-sm leading-6 rounded-full text-emerald-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20 bg-slate-100">
-            Using AI-Powered Tech to Fuel Your Move.{" "}
-            <Link href="#" className="font-semibold text-emerald-800">
-              <span className="absolute inset-0" aria-hidden="true" />
-              Read more <span aria-hidden="true">&rarr;</span>
-            </Link>
-          </div>
+        <div className="relative px-3 py-1 my-8 text-sm leading-6 rounded-full text-emerald-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20 bg-slate-100">
+          Using AI-Powered Tech to Fuel Your Move.{" "}
+          <Link href="#" className="font-semibold text-emerald-800">
+            <span className="absolute inset-0" aria-hidden="true" />
+            Read more <span aria-hidden="true">&rarr;</span>
+          </Link>
         </div>
         <div className="text-center">
           <h1 className="text-6xl font-bold tracking-wide sm:text-6xl">
